f894b65baa6d529e910211e863d913e2ba4bde88
RuoluYe <yeruolu@uw.edu>
2023-08-04 20:19:44 -0700

    keep loggin state & auto log out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useCallback, useEffect } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 import MainNavigation from "./shared/components/Navigation/MainNavigation/MainNavigation";
@@ -12,8 +12,62 @@ const NewDog = React.lazy(() => import("./dogs/pages/NewDog"));
 const UpdateDog = React.lazy(() => import("./dogs/pages/UpdateDog"));
 const Auth = React.lazy(() => import("./user/pages/Auth/Auth"));
 
+let logoutTimer;
+
 function App() {
-  const { userId, token, login, logout } = useAuth();
+  const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState()
+  const [userId, setUserId] = useState(false);
+
+  const login = useCallback((uid, token, expirationDate) => {
+    setToken(token);
+    setUserId(uid);
+    // log out time
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString()
+      })
+    );
+  }, []);
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setUserId(false);
+    localStorage.removeItem('userData')
+  }, []);
+  
+  // auto log out, when log out, clear timer, when log in, set a new timer
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer =setTimeout(logout, remainTime)
+    } else { // manual logged out 
+      clearTimeout(logoutTimer)
+    }
+  }, [token, logout, tokenExpirationDate])
+
+  // keep loggin in
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
+    }
+  }, [login]);
+
   let routes;
 
   if (token) {
