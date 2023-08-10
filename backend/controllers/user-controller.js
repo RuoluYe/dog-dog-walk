c55ec220849ffd2f5033ddb8c74d16b1a6e6fbd9
RuoluYe <yeruolu@uw.edu>
2023-08-10 10:37:51 -0700

    process.env

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -66,7 +66,7 @@ const signup = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: createdUser.discriminator, email: createdUser.email },
-      "yaoshi",
+      process.env.JWT_KEY,
       { expiresIn: "1h" }
     );
   } catch (err) {
@@ -114,7 +114,7 @@ const login = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: existingUser.discriminator, email: existingUser.email },
-      "yaoshi",
+      process.env.JWT_KEY,
       { expiresIn: "1h" }
     );
   } catch (err) {
