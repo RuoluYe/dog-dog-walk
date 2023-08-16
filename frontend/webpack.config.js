a5e450ec486d697748993c834efa6f953a36e695
RuoluYe <yeruolu@uw.edu>
2023-08-15 17:30:01 -0700

    .env webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,25 @@
+const xxhash = require('xxhash');
+
+module.exports = {
+  output: {
+    hashFunction: xxhash.hash64Hex,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
